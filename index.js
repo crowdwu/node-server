a13acc2ef26e0f188981faaf1ba1e5230083f392
crowdwu <48405664+crowdwu@users.noreply.github.com>
2019-04-28 10:21:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,40 @@ var url = require('url')
 var server= http.createServer(function(request,response){
     setTimeout(function(){
         response.setHeader('Content-Type','text/html; charset=utf-8')
-        response.writeHead(200, 'hha')
-        var pathObj = url.parse(request.url,true)
+        response.writeHead(200, 'haha')
+        var pathObj = url.parse(request.url,true) 
         console.log(pathObj) 
-        console.log(request.url)
+        console.log(request.url)  
+        //解析路由
         switch(pathObj.pathname){
-          case '/user/123':
+          case '/getTemperature':  //url有/getTemperature后缀返回数据
+                var ret
+                if(pathObj.query.city == 'Fujian'){
+                  ret = {
+                    city: 'Fujian',
+                    temperature: '25'
+                  }
+                }else{
+                  ret = {
+                    city: pathObj.query.city,
+                    temperature: '我啷个晓得'
+                  }
+                }
+                response.end(JSON.stringify(ret))
+                break;
+
+
+          case '/user/123':    //url有/user/123后缀返回README文件
                 response.end( fs.readFileSync(__dirname + '/README.md') )
                 break;
+
+         
+
+                //默认返回静态文件
             default:  response.end(fs.readFileSync(__dirname+'/sample'+'/test.html'))
     }}),2000})
 console.log('open http://localhost:8080')
 server.listen(8080)
+
+
+
